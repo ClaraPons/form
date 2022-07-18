92fb71eaf65671faf16ff4765e21765858c2c919
Clara Pons <clarapons@macbook-air-de-clara.home>
2022-07-18 16:03:54 +0200

    02 - Form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 
+const regex = new RegExp (/^[\w\.]+@([\w-]+\.)+[\w-]{2,4}$/)
 
 class App extends React.Component {
   constructor() {
@@ -16,25 +17,55 @@ class App extends React.Component {
     }
   }
 
-  handleEmailChange =()
-    
+  handleEmailChange = (e) => {
+      this.setState({
+        email: e.target.value
+      })
+    regex.test(e.target.value) && this.setState({ emailIsValid: true})
+    console.log(this.state.emailIsValid)
+  }
+
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value
+    })
+    e.target.value.length > 5 && this.setState({ passwordIsValid: true}, () =>{
+      console.log(this.state.passwordIsValid)
+    })
+  }
   
+  handleRememberMeChange = (e) => {
+    this.setState({
+      rememberMe: !this.state.rememberMe
+    }, () => {
+      console.log(this.state.rememberMe)
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault(
+      this.state.emailIsValid && this.state.passwordIsValid && this.setState({ isSubmitted: true}, () =>{
+        console.log(this.state.isSubmitted)
+      })
+    )
+  }
+
   render (){
     return(
       <div  class="m-5">
       <h1 class="text-center">Login</h1>
-      <form class="was-validated d-flex flex-column align-items-center" >
+      <form class="was-validated d-flex flex-column align-items-center" onSubmit={this.handleSubmit} >
             <div class="col-md-4 mb-3">
               <label for="validationServer01" class="form-label">Email address</label>
-              <input type="email" class="form-control is-valid" id="validationServer01"  required/>
+              <input type="email" class="form-control is-valid" id="validationServer01" onChange={this.handleEmailChange} required/>
             </div>
             <div class="col-md-4 mb-4">
               <label for="validationServer01" class="form-label">Password</label>
-              <input type="text" class="form-control is-valid" id="validationServer01"  required/>
+              <input type="password" class="form-control is-valid" id="validationServer01" onChange={this.handlePasswordChange} required/>
             </div>
             <div class="form-check mb-3">
-              <input type="checkbox" class="form-check-input" id="validationFormCheck1" required />
-              <label class="form-check-label" for="validationFormCheck1">Check this checkbox</label>
+              <input type="checkbox" class="form-check-input" id="validationFormCheck1" />
+              <label class="form-check-label" for="validationFormCheck1">Remember me</label>
             </div>
             <div class="mb-3">
               <button class="btn btn-primary" type="submit" >Submit</button>
